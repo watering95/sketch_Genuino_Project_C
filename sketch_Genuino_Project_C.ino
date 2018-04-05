3401ffe8a954f49a155a91da65e45a198d63643e
Unknown <watering95@gmail.com>
2018-04-05 21:08:13 +0900

    v.0.9 auto function

--- sketch_Genuino_Project_C.ino
+++ sketch_Genuino_Project_C.ino
@@ -25,11 +25,12 @@ Adafruit_DCMotor *motor2 = AFMS.getMotor(4);
 #define PIN_LATCH 12
 #endif
 
-#define MOTOR_FORWARD 1
-#define MOTOR_BACKWARD  4
-#define MOTOR_LEFTTURN  3
-#define MOTOR_RIGHTTURN 2
+#define MOTOR_FORWARD 2
+#define MOTOR_BACKWARD  5
+#define MOTOR_LEFTTURN  4
+#define MOTOR_RIGHTTURN 3
 #define MOTOR_STOP  0
+#define MOTOR_RUN   1
 
 BLEService machineService("BBB0");
 BLECharacteristic machineStateChara("BBB1", BLERead | BLENotify, 30);
@@ -38,17 +39,23 @@ BLEService motorService("0174");
 BLEIntCharacteristic directionChara("0175", BLEWrite);
 BLEIntCharacteristic speedLeftChara("0176", BLEWrite);
 BLEIntCharacteristic speedRightChara("0177", BLEWrite);
+BLEIntCharacteristic isAutoChara("0178", BLEWrite);
 
 boolean isConnectedCentral = false;
-unsigned int motorDirection = 0;
+boolean isAdjusted = false;
+boolean isAuto = false;
 unsigned int motorLeftSpeed = 0;
 unsigned int motorRightSpeed = 0;
+unsigned int setLeftSpeed = 0;
+unsigned int setRightSpeed = 0;
+unsigned int motorDirection = MOTOR_FORWARD;
 unsigned int motorState = MOTOR_STOP;
 char machineState[30];
-const int timeIMU = 10;
 const int timeBLE = 1000;
+const int adjustSpeed = 50;
 
 float gx, gy, gz, ax, ay, az;
+int controlDirection;
 
 void setup() {
   // put your setup code here, to run once:
@@ -65,6 +72,48 @@ void loop() {
   // put your main code here, to run repeatedly:
   BLE.poll();
   readIMU();
+  if(isAuto) autoRun();
+}
+
+void autoRun() {
+  changeMotorDirection();
+  if(controlDirection < 0) {
+     motorLeftSpeed = adjustSpeed;
+     motorRightSpeed = 0;
+     isAdjusted = false;
+  }
+  else if(controlDirection > 0) {
+     motorLeftSpeed = 0;
+     motorRightSpeed = adjustSpeed;
+     isAdjusted = false;
+  }
+  else {
+    motorLeftSpeed = 0;
+    motorRightSpeed = 0;
+    isAdjusted = true;
+  }
+  if(motorState != MOTOR_STOP) {
+    motorLeftSpeed += setLeftSpeed;
+    motorRightSpeed += setRightSpeed;
+  }
+  changeRunState();
+}
+
+void changeMotorDirection() {
+  switch(motorDirection) {
+    case MOTOR_FORWARD:
+      controlDirection = ay;
+      break;
+    case MOTOR_BACKWARD:
+      controlDirection = ay;
+      break;
+    case MOTOR_LEFTTURN:
+      controlDirection = ax;
+      break;
+    case MOTOR_RIGHTTURN:
+      controlDirection = ax;
+      break;
+  }   
 }
 
 void readIMU() {
@@ -128,6 +177,7 @@ void motorBack(unsigned int vl, unsigned int vr) {
 #endif
   Serial.print("Motor Run : ");
   Serial.println(dir);
+  motorDirection = MOTOR_BACKWARD;
   motorState = MOTOR_BACKWARD;
 }
 
@@ -147,6 +197,7 @@ void motorStop() {
 #endif
   Serial.print("Motor Stop : ");
   Serial.println(dir);
+  motorDirection = MOTOR_FORWARD;
   motorState = MOTOR_STOP;
 }
 
@@ -169,7 +220,8 @@ void motorRun(unsigned int vl, unsigned int vr) {
 #endif
   Serial.print("Motor Back : ");
   Serial.println(dir);
-  motorState = MOTOR_FORWARD;
+  motorDirection = MOTOR_FORWARD;
+  motorState = MOTOR_RUN;
 }
 
 void motorLeft(unsigned int vl, unsigned int vr) {
@@ -191,6 +243,7 @@ void motorLeft(unsigned int vl, unsigned int vr) {
 #endif
   Serial.print("Motor Left : ");
   Serial.println(dir);
+  motorDirection = MOTOR_LEFTTURN;
   motorState = MOTOR_LEFTTURN;
 }
 
@@ -213,6 +266,7 @@ void motorRight(unsigned int vl, unsigned int vr) {
 #endif
   Serial.print("Motor Right : ");
   Serial.println(dir);
+  motorDirection = MOTOR_RIGHTTURN;
   motorState = MOTOR_RIGHTTURN;
 }
 
@@ -240,6 +294,7 @@ void initBLE() {
   motorService.addCharacteristic(directionChara);
   motorService.addCharacteristic(speedLeftChara);
   motorService.addCharacteristic(speedRightChara);
+  motorService.addCharacteristic(isAutoChara);
 
   BLE.addService(machineService);
   BLE.addService(motorService);
@@ -250,6 +305,7 @@ void initBLE() {
   directionChara.setEventHandler(BLEWritten, directionCharacteristicWritten);
   speedLeftChara.setEventHandler(BLEWritten, speedLeftCharacteristicWritten);
   speedRightChara.setEventHandler(BLEWritten, speedRightCharacteristicWritten);
+  isAutoChara.setEventHandler(BLEWritten, isAutoCharacteristicWritten);
   
   BLE.advertise();
   Serial.println("BLE Genuino101 Peripheral");
@@ -283,7 +339,7 @@ void blePeripheralDisconnectHandler(BLEDevice central) {
 
 void speedLeftCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
   Serial.print("speedLeftCharacteristic event, written : ");
-  motorLeftSpeed = speedLeftChara.value();
+  setLeftSpeed = speedLeftChara.value();
   Serial.println(motorLeftSpeed);
 
   changeRunState();
@@ -291,7 +347,7 @@ void speedLeftCharacteristicWritten(BLEDevice central, BLECharacteristic charact
 
 void speedRightCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
   Serial.print("speedRightCharacteristic event, written : ");
-  motorRightSpeed = speedRightChara.value();
+  setRightSpeed = speedRightChara.value();
   Serial.println(motorRightSpeed);
 
   changeRunState();
@@ -305,13 +361,21 @@ void directionCharacteristicWritten(BLEDevice central, BLECharacteristic charact
   changeRunState();
 }
 
+void isAutoCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
+  Serial.print("isAutoCharacteristic event, written : ");
+  if(directionChara.value() == 1) isAuto = true;
+  else isAuto = false;
+}
+
 void changeRunState() {
-  switch (motorDirection) {
+  switch (motorState) {
     case MOTOR_STOP:
       motorStop();
       break;
-    case MOTOR_FORWARD:
-      motorRun(motorLeftSpeed,motorRightSpeed);
+    case MOTOR_RUN:
+      if(isAdjusted || !isAuto) {
+        motorRun(motorLeftSpeed,motorRightSpeed);
+      }
       break;
     case MOTOR_RIGHTTURN:
       motorRight(motorLeftSpeed,motorRightSpeed);
@@ -320,7 +384,9 @@ void changeRunState() {
       motorLeft(motorLeftSpeed,motorRightSpeed);
       break;
     case MOTOR_BACKWARD:
-      motorBack(motorLeftSpeed,motorRightSpeed);
+      if(isAdjusted || !isAuto) {
+        motorBack(motorLeftSpeed,motorRightSpeed);        
+      }
       break;
     default:
       motorStop();
